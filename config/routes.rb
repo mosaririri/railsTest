e0335cbf3913a804b92757fa8e4a2adfea69e577
mosaririri <dos.quoi813@gmail.com>
2019-09-10 22:40:05 +0900

    create top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  get 'static_pages/home'
   get 'top/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
