e3fefaf1880952d70cf9e35508aab70ed89c142b
mosaririri <dos.quoi813@gmail.com>
2019-09-29 17:32:54 +0900

    create user new page

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class TopController < ApplicationController
-  def index; end
+  def index
+    @user = User.first 
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get 'top/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ get '/signup', to: 'users#new'
+ post '/signup', to: 'users#create'
 end
