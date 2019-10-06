96476dce53125d3b5dab93d3b1cebaae9440410e
mosaririri <dos.quoi813@gmail.com>
2019-10-06 22:42:57 +0900

    top form get all users

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get 'static_pages/home'
   get 'top/index'
+  get 'users/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  get '/signup', to: 'users#new'
  post '/signup', to: 'users#create'
