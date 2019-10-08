e7d6923c59f66d5497899635517eef9ceec4f4ac
mosaririri <dos.quoi813@gmail.com>
2019-10-08 23:11:34 +0900

    create edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,15 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+
   private
 
   def user_params
