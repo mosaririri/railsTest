cf140d5457284bb6f5d0ca3f77573c0008bfd6e8
mosaririri <dos.quoi813@gmail.com>
2019-10-14 22:23:15 +0900

    Userモデルの簡単なテストを実施

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true.
 # ユーザーモデル
 class User < ApplicationRecord
+  has_many :tasks, dependent: :destroy
   validates :name, presence: true
   validates :email, presence: true
   validates :password, presence: true
